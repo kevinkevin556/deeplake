a0a79fe7c6148fd025aed5cc231ba8becdae5a4c
Kevin Hong <kevinkevin556@yahoo.com.tw>
2023-10-29 02:07:18 +0800

    Create trainer and initializer class for MSKD module.

--- mskd.py
+++ mskd.py
@@ -1,24 +1,31 @@
 import argparse
 import os
+import random
+from copy import deepcopy
+from itertools import chain
 from pathlib import Path
-from typing import Optional
+from typing import Literal, Optional, Tuple, Union
 
 import numpy as np
 import torch
 import torch.nn.functional as F
-from monai.data import decollate_batch
+from medaset.transforms import ApplyMaskMappingd, BackgroundifyClassesd
+from monai.data import DataLoader, Dataset, decollate_batch
 from monai.inferers import sliding_window_inference
-from monai.losses import DiceCELoss
-from monai.metrics import DiceMetric
+from monai.losses import DiceCELoss, DiceLoss, FocalLoss
+from monai.metrics import DiceMetric, Metric
 from monai.networks.nets import BasicUNet
 from monai.networks.utils import one_hot
-from monai.transforms import AsDiscrete, Compose
+from monai.transforms import AddChanneld, AsDiscrete, Compose, LoadImaged, ToTensord
 from torch import nn, ones, zeros
 from torch.nn import BCEWithLogitsLoss
 from torch.optim import SGD, Adam, AdamW
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.utils.data import ConcatDataset, Subset
 from torch.utils.tensorboard import SummaryWriter
 from torchtyping import TensorType
 from tqdm import tqdm
+from tqdm.auto import tqdm
 
 
 # Type Annotation
@@ -162,69 +169,146 @@ class MSKDModule(nn.Module):
 
     def load(self, checkpoint_dir):
         try:
-            self.feat_extractor.load_state_dict(torch.load(os.path.join(checkpoint_dir, "ct_teacher_state.pth")))
-            self.predictor.load_state_dict(torch.load(os.path.join(checkpoint_dir, "predictor_state.pth")))
+            self.teacher0.load_state_dict(torch.load(os.path.join(checkpoint_dir, "teacher0_state.pth")))
+            self.teacher1.load_state_dict(torch.load(os.path.join(checkpoint_dir, "teacher1_state.pth")))
             self.student.load_state_dict(torch.load(os.path.join(checkpoint_dir, "student_state.pth")))
         except Exception as e:
             raise e
 
+    def print_info(self):
+        print("Module Teacher-0:", self.teacher0.__class__.__name__)
+        print("       Teacher-1:", self.teacher1.__class__.__name__)
+        print("       Student:", self.student.__class__.__name__)
+        print("Optimizer:", self.optimizer.__class__.__name__, f"(lr = {self.lr})")
+        print(f"Loss function: {self.kl_loss.__class__.__name__}")
 
-class SimpleTeacherTrainer:
+
+class MSKDTrainer:
     def __init__(
         self,
-        max_iter=40000,
-        metric=DiceMetric(include_background=True, reduction="mean", get_not_nans=False),
-        eval_step=500,
-        checkpoint_dir="./checkpoints/",
-        device="cuda",
+        num_classes: int,
+        max_iter: int = 10000,
+        metric: Metric = DiceMetric(include_background=True, reduction="mean", get_not_nans=False),
+        eval_step: int = 100,
+        checkpoint_dir: str = "./checkpoints/",
+        device: Literal["cuda", "cpu"] = "cuda",
+        pretrained_dir: str = None,
     ):
+        self.num_classes = num_classes
         self.max_iter = max_iter
         self.metric = metric
         self.eval_step = eval_step
         self.checkpoint_dir = checkpoint_dir
         Path(self.checkpoint_dir).mkdir(parents=True, exist_ok=True)
         self.device = device
-
-    def show_training_info(self, module, ct_train_dtl, ct_val_dtl, mr_train_dtl, mr_val_dtl):
-        print("--------")
-        print("Device:", self.device)  # device is a global variable (not an argument of cli)
-        print("# of Training Samples:", {"ct": len(ct_train_dtl), "mr": len(mr_train_dtl)})
-        print("# of Validation Samples:", {"ct": len(ct_val_dtl), "mr": len(mr_val_dtl)})
-        print("Max iteration:", self.max_iter, f"steps (validates per {self.eval_step} steps)")
-        print("Checkpoint directory:", self.checkpoint_dir)
-        print("Module Encoder:", module.feat_extractor.__class__.__name__)
-        print("       Decoder:", module.predictor.__class__.__name__)
-        print("Optimizer:", module.optimizer.__class__.__name__, f"(lr = {module.lr})")
-        print("Loss function:", {"ct": module.ct_tal.__class__.__name__, "mr": module.mr_tal.__class__.__name__})
-        print("Evaluation metric:", self.metric.__class__.__name__)
-        print("--------")
-
-    def validation(self, module, ct_dtl, mr_dtl, global_step=None, **kwargs):
-        module.eval()
-        val_metrics = []
-        num_classes = module.num_class
-        val_pbar = tqdm(range(len(ct_dtl) + len(mr_dtl)), dynamic_ncols=True)
-        metric_name = self.metric.__class__.__name__
-        train_val_desc = "Validate ({} Steps) ({}={:2.5f})"
-        simple_val_desc = "Validate ({}={:2.5f})"
-        post = {
-            "x": Compose(AsDiscrete(argmax=True, to_onehot=num_classes)),
-            "y": Compose(AsDiscrete(to_onehot=num_classes)),
+        self.pretrained_modules = PretrainedModules(
+            num_classes=num_classes,
+            max_iter=max_iter,
+            metric=DiceMetric(),
+            eval_step=100,
+            checkpoint_dir=pretrained_dir if pretrained_dir else checkpoint_dir,
+            device=device,
+        )
+        self.postprocess = {
+            "x": AsDiscrete(argmax=True, to_onehot=num_classes),
+            "y": AsDiscrete(to_onehot=num_classes),
         }
 
-        with torch.no_grad():
-            for i in val_pbar:
-                # Set dataloader and post-processing
-                if i % 2 == 0:  # ct
-                    batch = next(iter(ct_dtl))
-                else:  # mr
-                    batch = next(iter(mr_dtl))
+    # auxilary function to show training info before training procedure starts
+    def show_training_info(self, module, train_dataloader, val_dataloader):
+        print("--------")
+        print("Device:", self.device)  # device is a global variable (not an argument of cli)
+        print("# of Training Samples:", {"ct": len(train_dataloader[0]), "mr": len(train_dataloader[1])})
+        print("# of Validation Samples:", {"ct": len(val_dataloader[0]), "mr": len(val_dataloader[1])})
+        print("Max iteration:", self.max_iter, f"steps (validates per {self.eval_step} steps)")
+        print("Checkpoint directory:", self.checkpoint_dir)
+        print("Evaluation metric:", self.metric.__class__.__name__)
+        module.print_info()
+        print("--------")
 
+    def pretrain_single_organ(self, module_id, train_dataloader, val_dataloader):
+        assert module_id in [0, 1], f"Invalid module id. Got {module_id}, expect {0, 1}"
+        self.pretrained_modules.train(module_id, train_dataloader, val_dataloader)
+        self.pretrained_modules.load(module_id)
+        torch.cuda.empty_cache()
+        if module_id == 0:
+            for param in self.pretrained_modules.pretrained_module0.parameters():
+                param.requires_grad = False
+        else:
+            for param in self.pretrained_modules.pretrained_module1.parameters():
+                param.requires_grad = False
+
+    def distillate_teachers_into_student(self, module, train_dataloader, val_dataloader):
+        best_metric = 0
+        train_pbar = tqdm(range(self.max_iter), dynamic_ncols=True)
+        writer = SummaryWriter(log_dir=self.checkpoint_dir)
+        writer.add_scalar(f"train/{self.metric.__class__.__name__}", best_metric, 0)
+
+        iter_count = 0
+        train_iterator = chain(*train_dataloader)  # Concatenate two iterables (dataloaders) into one
+        for step in train_pbar:
+            ## Train
+            module.train()
+            try:
+                batch = next(train_iterator)
+            except StopIteration:
+                # If the iterator runs out of data, create a new one.
+                iter_count = 0
+                train_iterator = chain(*train_dataloader)
+                batch = next(train_iterator)
+            image, mask = batch["image"].to(self.device), batch["label"].to(self.device)
+            train_loss = module.update(x=image)
+            train_pbar.set_description(f"Training ({step+1} / {self.max_iter} Steps) (loss = {train_loss:2.5f})")
+            writer.add_scalar(f"train/train_loss", train_loss, step)
+
+            ## Validation
+            if (step + 1) % self.eval_step == 0 or (step + 1) == self.max_iter:
+                val_metric = self.validation(module, val_dataloader, step)
+                writer.add_scalar(f"train/{self.metric.__class__.__name__}", val_metric, step)
+                if val_metric > best_metric:
+                    module.save(self.checkpoint_dir)
+                    tqdm.write(f"Model saved! Validation: (New) {val_metric:2.7f} > (Old) {best_metric:2.7f}")
+                    best_metric = val_metric
+                else:
+                    tqdm.write(f"No improvement. Validation: (New) {val_metric:2.7f} <= (Old) {best_metric:2.7f}")
+
+    def train(self, module, train_dataloader, val_dataloader, load_pretrained: Optional[Union[list, dict]] = None):
+        ## Load or pretrain modules
+        for p_id in [0, 1]:
+            if isinstance(load_pretrained, dict) and p_id in load_pretrained.keys():
+                self.pretrained_modules.load(p_id, load_pretrained[p_id])
+            elif isinstance(load_pretrained, (list, tuple)) and p_id in load_pretrained:
+                self.pretrained_modules.load(p_id)
+            else:
+                self.pretrain_single_organ(p_id, train_dataloader[p_id], val_dataloader[p_id])
+
+        # Assign pretrained modules as teachers in mskd module
+        module.teacher0.load_state_dict(self.pretrained_modules.pretrained_module0.net.state_dict())
+        module.teacher1.load_state_dict(self.pretrained_modules.pretrained_module1.net.state_dict())
+
+        ## Train with mskd module
+        self.show_training_info(module, train_dataloader, val_dataloader)
+        self.distillate_teachers_into_student(module, train_dataloader, val_dataloader)
+
+    def validation(self, module, dataloader, global_step=None, **kwargs):
+        module.eval()
+        val_metrics = []
+        val_pbar = tqdm(
+            iterable=chain(*dataloader),
+            total=sum([len(d) for d in dataloader]),
+            dynamic_ncols=True,
+        )
+        metric_name = self.metric.__class__.__name__
+        train_val_desc = "Validate ({} Steps) ({}={:2.5f})"  # progress bar description used during training
+        simple_val_desc = "Validate ({}={:2.5f})"  # progress bar description used when the network is tested
+        with torch.no_grad():
+            for batch in val_pbar:
                 # Infer, decollate data into list of samples, and proprocess both predictions and labels
                 images, masks = batch["image"].to(self.device), batch["label"].to(self.device)
-                samples = decollate_batch({"prediction": module.inference(images), "ground_truth": masks})
-                outputs = [post["x"](sample["prediction"]) for sample in samples]
-                masks = [post["y"](sample["ground_truth"]) for sample in samples]
+                infer_out = module.inference(images)
+                samples = decollate_batch({"prediction": infer_out, "ground_truth": masks})
+                outputs = [self.postprocess["x"](sample["prediction"]) for sample in samples]
+                masks = [self.postprocess["y"](sample["ground_truth"]) for sample in samples]
                 # Compute validation metrics
                 self.metric(y_pred=outputs, y=masks)
                 batch_metric = self.metric.aggregate().item()
@@ -238,29 +322,34 @@ class SimpleTeacherTrainer:
         mean_val_metric = np.mean(val_metrics)
         return mean_val_metric
 
-    def train(self, module, ct_train_dtl, ct_val_dtl, mr_train_dtl, mr_val_dtl):
-        self.show_training_info(module, ct_train_dtl, ct_val_dtl, mr_train_dtl, mr_val_dtl)
-        best_metric = 0
-        train_pbar = tqdm(range(self.max_iter), dynamic_ncols=True)
-        writer = SummaryWriter(log_dir=self.checkpoint_dir)
-        writer.add_scalar(f"train/{self.metric.__class__.__name__}", 0, 0)  # validation metric starts from zero
 
-        for step in train_pbar:
-            module.train()
-            ct_batch = next(iter(ct_train_dtl))
-            mr_batch = next(iter(mr_train_dtl))
-            ct_image, ct_mask = ct_batch["image"].to(self.device), ct_batch["label"].to(self.device)
-            mr_image, mr_mask = mr_batch["image"].to(self.device), mr_batch["label"].to(self.device)
-            loss = module.update(ct_image, ct_mask, mr_image, mr_mask)
-            writer.add_scalar(f"train/seg_loss", loss, step)
-            train_pbar.set_description(f"Training ({step} / {self.max_iter} Steps) (loss={loss:2.5f})")
-            if ((step + 1) % self.eval_step == 0) or (step == self.max_iter - 1):
-                val_metric = self.validation(module, ct_val_dtl, mr_val_dtl, global_step=step)
-                writer.add_scalar(f"train/{self.metric.__class__.__name__}", val_metric, step)
-                if val_metric > best_metric:
-                    module.save(self.checkpoint_dir)
-                    print(f"Model saved! Validation: (New) {val_metric:2.7f} > (Old) {best_metric:2.7f}")
-                    best_metric = val_metric
-                else:
-                    print(f"No improvement. Validation: (New) {val_metric:2.7f} <= (Old) {best_metric:2.7f}")
-                    print(f"No improvement. Validation: (New) {val_metric:2.7f} <= (Old) {best_metric:2.7f}")
+class MSKDInitializer:
+    @staticmethod
+    def init_dataloaders(train_dataset, val_dataset, test_dataset, batch_size, dev):
+        train_dataloaders = tuple(
+            [DataLoader(train_dataset[i], batch_size=batch_size, shuffle=~dev, pin_memory=True) for i in range(2)]
+        )
+        val_dataloaders = tuple(
+            [DataLoader(val_dataset[i], batch_size=1, shuffle=False, pin_memory=True) for i in range(2)]
+        )
+        test_dataloaders = tuple(
+            [DataLoader(test_dataset[i], batch_size=1, shuffle=False, pin_memory=True) for i in range(2)]
+        )
+        return train_dataloaders, val_dataloaders, test_dataloaders
+
+    @staticmethod
+    def init_module(loss, optim, lr, dataset, modality, masked, device, **kwargs):
+        module = MSKDModule(num_classes=dataset["num_classes"], lr=lr, **kwargs)
+        return module
+
+    @staticmethod
+    def init_trainer(num_classes, max_iter, eval_step, checkpoint_dir, device, pretrained_dir=None):
+        trainer = CoTrainingTrainer(
+            num_classes=num_classes,
+            max_iter=max_iter,
+            eval_step=eval_step,
+            checkpoint_dir=checkpoint_dir,
+            device=device,
+            pretrained_dir=pretrained_dir,
+        )
+        return trainer
