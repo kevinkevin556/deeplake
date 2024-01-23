f5e234a93b1d1cbecc390d986345c0fb9b7487ab
Kevin Hong <kevinkevin556@yahoo.com.tw>
2024-01-24 05:49:42 +0800

    refactor(lib): remove obsolete or unused files
    BREAKING CHANGE:

--- lib/utils/util/__init__.py
+++
@@ -1,6 +0,0 @@
-# Acknowledgement:
-# The code contained in this file has been generously sourced from the GitHub repository:
-#   https://github.com/jhoffman/pytorch-CycleGAN-and-pix2pix
-#
-# and has been partially revised for this research.
-
