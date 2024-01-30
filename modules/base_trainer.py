4b86a6a94a6bdd0c49473e7f9c9a6d4866a09749
Kevin Hong <kevinkevin556@yahoo.com.tw>
2024-01-31 02:45:40 +0800

    feat(base): create a logger class to manage the summary writer and logging functionality

--- modules/base_trainer.py
+++ modules/base_trainer.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
 import warnings
+from pathlib import Path
 from typing import Literal, Union
 
 import numpy as np
 import tqdm
-from colorful import green, red
+from loguru import logger
 from monai.data import DataLoader as MonaiDataLoader
 from monai.metrics import DiceMetric, Metric
 from torch import nn
@@ -17,6 +18,66 @@ from modules.base_updater import BaseUpdater
 from modules.base_validator import BaseValidator
 
 DataLoader = Union[MonaiDataLoader, PyTorchDataLoader]
+
+
+def get_metric_str(metric, prefix=None, suffix=None):
+    metric = metric if isinstance(metric, str) else metric.__class__.__name__
+    metric = f"{prefix}/{metric}" if prefix is not None else metric
+    metric = f"{metric}:{suffix}" if suffix is not None else metric
+    return metric
+
+
+class TrainLogger:
+    def __init__(self, checkpoint_dir):
+        self.checkpoint_dir = checkpoint_dir
+        self.log_file = Path(self.checkpoint_dir) / "train.log"
+
+        self.writer = SummaryWriter(log_dir=checkpoint_dir)
+        self.msg = ""
+
+    def catch_msg(self, s):
+        self.msg = s[:-1]
+
+    def add_to_summary_writer(self, metric, value, step, prefix=None, suffix=None):
+        metric = get_metric_str(metric, prefix, suffix)
+        self.writer.add_scalar(metric, value, step)
+
+    def add_to_log_file(self, metric, value, step, prefix=None, suffix=None):
+        metric = get_metric_str(metric, prefix, suffix)
+        logger.remove()
+        logger.add(self.log_file)
+        logger.info(f"Step {step} | {metric} = {value}")
+
+    def log_train(self, metric, value, step):
+        self.add_to_summary_writer(metric, value, step, prefix="train")
+
+    def log_val(self, metric, suffix, value, step):
+        if isinstance(suffix, (list, tuple)):
+            logger.remove()
+            logger.add(self.log_file)
+            logger.info(f"Step {step} | Validation")
+            for suf, val in zip(suffix, value):
+                self.add_to_summary_writer(metric, val, step, prefix="val", suffix=suf)
+                self.add_to_log_file(metric, val, step, suffix=suf)
+        else:
+            self.add_to_summary_writer(metric, value, step, prefix="val", suffix=suffix)
+            self.add_to_log_file(metric, value, step, suffix=suffix)
+
+    def success(self, msg):
+        logger.remove()
+        logger.add(self.log_file)
+        logger.add(self.catch_msg, colorize=True)
+        logger.success(msg)
+        tqdm.write(self.msg)
+        self.msg = ""
+
+    def info(self, msg):
+        logger.remove()
+        logger.add(self.log_file)
+        logger.add(self.catch_msg, colorize=True)
+        logger.info(msg)
+        tqdm.write(self.msg)
+        self.msg = ""
 
 
 class BaseTrainer:
@@ -40,7 +101,7 @@ class BaseTrainer:
         else:
             self.validator = validator
         if dev:
-            self.max_iter = 3
+            self.max_iter = 10
             self.eval_step = 3
             self.checkpoint_dir = "./debug/"
             warnings.warn(
@@ -75,12 +136,12 @@ class BaseTrainer:
         train_dataloader: DataLoader | None = None,
         val_dataloader: DataLoader | None = None,
     ):
-        # Set up dataloaders and modules
         self.show_training_info(module, train_dataloader, val_dataloader)
 
         # Initalize progress bar and tensorboard writer
         train_pbar = tqdm(range(self.max_iter), dynamic_ncols=True)
-        writer = SummaryWriter(log_dir=self.checkpoint_dir)
+        # writer = SummaryWriter(log_dir=self.checkpoint_dir)
+        logger = TrainLogger(self.checkpoint_dir)
 
         # Initial stage. Note: updater(module) checks the module and returns a partial func of updating parameters.
         best_metric = 0
@@ -93,23 +154,24 @@ class BaseTrainer:
             batch = next(iter(train_dataloader))
             images, masks = batch["image"].to(self.device), batch["label"].to(self.device)
             modality_label = batch["modality"][0]
-            assert modality_label in ("ct", "mr")
+            assert modality_label in {"ct", "mr"}
             modality = 0 if modality_label == "ct" else 1
             loss = module_update(images, masks, modality)
 
             # Update progress bar and summary writer
             info = {"step": step + 1, "max_iter": self.max_iter, "modality_label": modality_label, "loss": loss}
             train_pbar.set_description(self.pbar_description.format(**info))
-            writer.add_scalar(f"train/{module.criterion.__class__.__name__}", loss, step)
+            logger.log_train(module.criterion, loss, step)
 
             # Validation
             if ((step + 1) % self.eval_step == 0) or (step == self.max_iter - 1):
                 val_metrics = self.validator(module, val_dataloader, global_step=step)
-
-                # Update summary writer
-                writer.add_scalar(f"val/{self.metric.__class__.__name__}:Average", val_metrics["mean"], step)
-                writer.add_scalar(f"val/{self.metric.__class__.__name__}:CT", val_metrics["ct"], step)
-                writer.add_scalar(f"val/{self.metric.__class__.__name__}:MR", val_metrics["mr"], step)
+                logger.log_val(
+                    self.metric,
+                    suffix=["Average", "CT", "MR"],
+                    value=(val_metrics["mean"], val_metrics["ct"], val_metrics["mr"]),
+                    step=step,
+                )
 
                 # Select validation metric
                 if min(val_metrics["ct"], val_metrics["mr"]) is not np.nan:
@@ -120,9 +182,13 @@ class BaseTrainer:
                 # Update best metric
                 if val_metric > best_metric:
                     module.save(self.checkpoint_dir)
-                    msg = green(f"Model saved! Validation: (New) {val_metric:2.7f} > (Old) {best_metric:2.7f}")
+                    logger.success(
+                        f"Model saved! Validation: (New) {val_metric:2.7f} > (Old) {best_metric:2.7f} "
+                        f"(CT) {val_metrics['ct']:2.7f} (MR) {val_metrics['mr']:2.7f}"
+                    )
                     best_metric = val_metric
                 else:
-                    msg = red(f"No improvement. Validation: (New) {val_metric:2.7f} <= (Old) {best_metric:2.7f}")
-                msg += f" (CT) {val_metrics['ct']:2.7f} (MR) {val_metrics['mr']:2.7f}"
-                tqdm.write(str(msg))
+                    logger.info(
+                        f"No improvement. Validation: (New) {val_metric:2.7f} <= (Old) {best_metric:2.7f} "
+                        f"(CT) {val_metrics['ct']:2.7f} (MR) {val_metrics['mr']:2.7f}"
+                    )
