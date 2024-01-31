1b247daf0010cd75926eff45f156527f36ef2a5f
Kevin Hong <kevinkevin556@yahoo.com.tw>
2024-01-31 21:10:37 +0800

    feat(lib): create classes of default transforms

--- lib/datasets/amos_default_transform.py
+++ lib/datasets/amos_default_transform.py
@@ -12,50 +12,64 @@ from monai.transforms import (
     Spacingd,
     SpatialPadd,
     ToTensord,
-)
-
-train_transform = Compose(
-    [
-        LoadImaged(keys=["image", "label"]),
-        EnsureChannelFirstd(keys=["image", "label"], strict_check=True, channel_dim="no_channel"),
-        Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
-        Orientationd(keys=["image", "label"], axcodes="RAS"),
-        ScaleIntensityRanged(keys=["image"], a_min=-125, a_max=275, b_min=0.0, b_max=1.0, clip=True),
-        CropForegroundd(keys=["image", "label"], source_key="image"),
-        RandCropByPosNegLabeld(
-            keys=["image", "label"],
-            label_key="label",
-            spatial_size=(96, 96, 96),
-            pos=1,
-            neg=1,
-            num_samples=2,
-            image_key="image",
-            image_threshold=0,
-            allow_smaller=True,
-        ),
-        SpatialPadd(keys=["image", "label"], spatial_size=(96, 96, 96)),
-        RandShiftIntensityd(keys=["image"], offsets=0.10, prob=0.50),
-        RandAffined(
-            keys=["image", "label"],
-            mode=("bilinear", "nearest"),
-            prob=1.0,
-            spatial_size=(96, 96, 96),
-            rotate_range=(0, 0, np.pi / 30),
-            scale_range=(0.1, 0.1, 0.1),
-        ),
-        ToTensord(keys=["image", "label"]),
-    ]
+    Transform,
 )
 
 
-test_transform = Compose(
-    [
-        LoadImaged(keys=["image", "label"]),
-        EnsureChannelFirstd(keys=["image", "label"], strict_check=True, channel_dim="no_channel"),
-        Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
-        Orientationd(keys=["image", "label"], axcodes="RAS"),
-        ScaleIntensityRanged(keys=["image"], a_min=-125, a_max=275, b_min=0.0, b_max=1.0, clip=True),
-        CropForegroundd(keys=["image", "label"], source_key="image"),
-        ToTensord(keys=["image", "label"]),
-    ]
-)
+class TrainTransform(Transform):
+    transform = Compose(
+        [
+            LoadImaged(keys=["image", "label"]),
+            EnsureChannelFirstd(keys=["image", "label"], strict_check=True, channel_dim="no_channel"),
+            Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+            Orientationd(keys=["image", "label"], axcodes="RAS"),
+            ScaleIntensityRanged(keys=["image"], a_min=-125, a_max=275, b_min=0.0, b_max=1.0, clip=True),
+            CropForegroundd(keys=["image", "label"], source_key="image"),
+            RandCropByPosNegLabeld(
+                keys=["image", "label"],
+                label_key="label",
+                spatial_size=(96, 96, 96),
+                pos=1,
+                neg=1,
+                num_samples=2,
+                image_key="image",
+                image_threshold=0,
+                allow_smaller=True,
+            ),
+            SpatialPadd(keys=["image", "label"], spatial_size=(96, 96, 96)),
+            RandShiftIntensityd(keys=["image"], offsets=0.10, prob=0.50),
+            RandAffined(
+                keys=["image", "label"],
+                mode=("bilinear", "nearest"),
+                prob=1.0,
+                spatial_size=(96, 96, 96),
+                rotate_range=(0, 0, np.pi / 30),
+                scale_range=(0.1, 0.1, 0.1),
+            ),
+            ToTensord(keys=["image", "label"]),
+        ]
+    )
+
+    def __call__(self, data):
+        return self.transform(data)
+
+
+class TestTransform(Transform):
+    transform = Compose(
+        [
+            LoadImaged(keys=["image", "label"]),
+            EnsureChannelFirstd(keys=["image", "label"], strict_check=True, channel_dim="no_channel"),
+            Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+            Orientationd(keys=["image", "label"], axcodes="RAS"),
+            ScaleIntensityRanged(keys=["image"], a_min=-125, a_max=275, b_min=0.0, b_max=1.0, clip=True),
+            CropForegroundd(keys=["image", "label"], source_key="image"),
+            ToTensord(keys=["image", "label"]),
+        ]
+    )
+
+    def __call__(self, data):
+        return self.transform(data)
+
+
+train_transform = TrainTransform()
+test_transform = TestTransform()
--- lib/datasets/smat_default_transform.py
+++ lib/datasets/smat_default_transform.py
@@ -11,36 +11,50 @@ from monai.transforms import (
     SpatialCropd,
     SpatialPadd,
     ToTensord,
-)
-
-ct_transform = Compose(
-    [
-        LoadImaged(
-            keys=["image", "label"],
-            reader=[PydicomReader(swap_ij=False), CV2Reader(flags=cv2.IMREAD_GRAYSCALE)],
-        ),
-        EnsureChannelFirstd(keys=["image", "label"], channel_dim="no_channel"),
-        ScaleIntensityRanged(keys=["image"], a_min=-200, a_max=200, b_min=0, b_max=1, clip=True),
-        CropForegroundd(keys=["image", "label"], source_key="label"),
-        SpatialPadd(keys=["image", "label"], spatial_size=(512, 512)),
-        SpatialCropd(keys=["image", "label"], roi_center=(256, 256), roi_size=(512, 512)),
-        ToTensord(keys=["image", "label"]),
-    ]
+    Transform,
 )
 
 
-mr_transform = Compose(
-    [
-        LoadImaged(
-            keys=["image", "label"],
-            reader=[PydicomReader(swap_ij=False, force=True), CV2Reader(flags=cv2.IMREAD_GRAYSCALE)],
-            image_only=True,
-        ),
-        EnsureChannelFirstd(keys=["image", "label"], channel_dim="no_channel"),
-        ScaleIntensityRanged(keys=["image"], a_min=0, a_max=1000, b_min=0, b_max=1, clip=True),
-        CropForegroundd(keys=["image", "label"], source_key="label"),
-        Resized(keys=["image", "label"], spatial_size=512, size_mode="longest", mode="nearest-exact"),
-        SpatialPadd(keys=["image", "label"], spatial_size=(512, 512)),
-        ToTensord(keys=["image", "label"]),
-    ]
-)
+class CtTransform(Transform):
+    transform = Compose(
+        [
+            LoadImaged(
+                keys=["image", "label"],
+                reader=[PydicomReader(swap_ij=False), CV2Reader(flags=cv2.IMREAD_GRAYSCALE)],
+            ),
+            EnsureChannelFirstd(keys=["image", "label"], channel_dim="no_channel"),
+            ScaleIntensityRanged(keys=["image"], a_min=-200, a_max=200, b_min=0, b_max=1, clip=True),
+            CropForegroundd(keys=["image", "label"], source_key="label"),
+            SpatialPadd(keys=["image", "label"], spatial_size=(512, 512)),
+            SpatialCropd(keys=["image", "label"], roi_center=(256, 256), roi_size=(512, 512)),
+            ToTensord(keys=["image", "label"]),
+        ]
+    )
+
+    def __call__(self, data):
+        return self.transform(data)
+
+
+class MrTransform(Transform):
+    transform = Compose(
+        [
+            LoadImaged(
+                keys=["image", "label"],
+                reader=[PydicomReader(swap_ij=False, force=True), CV2Reader(flags=cv2.IMREAD_GRAYSCALE)],
+                image_only=True,
+            ),
+            EnsureChannelFirstd(keys=["image", "label"], channel_dim="no_channel"),
+            ScaleIntensityRanged(keys=["image"], a_min=0, a_max=1000, b_min=0, b_max=1, clip=True),
+            CropForegroundd(keys=["image", "label"], source_key="label"),
+            Resized(keys=["image", "label"], spatial_size=512, size_mode="longest", mode="nearest-exact"),
+            SpatialPadd(keys=["image", "label"], spatial_size=(512, 512)),
+            ToTensord(keys=["image", "label"]),
+        ]
+    )
+
+    def __call__(self, data):
+        return self.transform(data)
+
+
+ct_transform = CtTransform()
+mr_transform = MrTransform()
