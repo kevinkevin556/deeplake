339a2fa905382f61fb4c8f739490b0223f2b39d1
Kevin Hong <kevinkevin556@yahoo.com.tw>
2024-01-31 12:56:25 +0800

    feat(datasets): create wrapper classes for smat datasets and default transformation classes

---
+++ lib/datasets/smat.py
@@ -0,0 +1,194 @@
+from __future__ import annotations
+
+import numpy as np
+from medaset.lake import SmatCtDataset, SmatMrDataset
+
+from .dataset_wrapper import Dataset
+
+
+class _SmatCtDatasetWithBackgroundInfo(SmatCtDataset):
+    def __init__(
+        self,
+        *args,
+        background_classes: list | None = None,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        if background_classes:
+            self.background_classes = background_classes
+        else:
+            self.background_classes = [0]
+
+    def __getitem__(self, index):
+        output = super().__getitem__(index)
+        if isinstance(output, dict):
+            output["num_classes"] = self.num_classes
+            output["background_classes"] = np.array(self.background_classes)
+        if isinstance(output, list):
+            for item in output:
+                item["num_classes"] = self.num_classes
+                item["background_classes"] = np.array(self.background_classes)
+        return output
+
+
+class _SmatMrDatasetWithBackgroundInfo(SmatMrDataset):
+    def __init__(
+        self,
+        *args,
+        background_classes: list | None = None,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        if background_classes:
+            self.background_classes = background_classes
+        else:
+            self.background_classes = [0]
+
+    def __getitem__(self, index):
+        output = super().__getitem__(index)
+        if isinstance(output, dict):
+            output["num_classes"] = self.num_classes
+            output["background_classes"] = np.array(self.background_classes)
+        if isinstance(output, list):
+            for item in output:
+                item["num_classes"] = self.num_classes
+                item["background_classes"] = np.array(self.background_classes)
+        return output
+
+
+class SmatCtDataset(Dataset):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.num_classes != 4:
+            raise ValueError(f"There are 4 classes in SMAT dataset. Got {self.num_classes}")
+
+        if self.in_use:
+            self.train_dataset = _SmatCtDatasetWithBackgroundInfo(
+                root_dir=self.root_dir,
+                target="all",
+                stage="train",
+                transform=self.train_transform,
+                mask_mapping={c: 0 for c in self.train_background_classes},
+                cache_rate=self.cache_rate,
+                num_workers=self.num_workers,
+                dev=self.dev,
+                random_seed=self.random_seed,
+                split_ratio=(
+                    (1 - self.holdout_ratio) ** 2,
+                    (1 - self.holdout_ratio) * self.holdout_ratio,
+                    self.holdout_ratio,
+                ),
+                sm_as_whole=True,
+                background_classes=self.train_background_classes,
+            )
+            setattr(self.train_dataset, "batch_size", self.train_batch_size)
+
+            self.val_dataset = _SmatCtDatasetWithBackgroundInfo(
+                root_dir=self.root_dir,
+                target="all",
+                stage="validation",
+                transform=self.test_transform,
+                mask_mapping={c: 0 for c in self.train_background_classes},
+                cache_rate=self.cache_rate,
+                num_workers=self.num_workers,
+                dev=self.dev,
+                random_seed=self.random_seed,
+                split_ratio=(
+                    (1 - self.holdout_ratio) ** 2,
+                    (1 - self.holdout_ratio) * self.holdout_ratio,
+                    self.holdout_ratio,
+                ),
+                sm_as_whole=True,
+                background_classes=self.train_background_classes,
+            )
+
+            self.test_dataset = _SmatCtDatasetWithBackgroundInfo(
+                root_dir=self.root_dir,
+                stage="test",
+                target="all",
+                transform=self.test_transform,
+                mask_mapping={c: 0 for c in self.test_background_classes},
+                cache_rate=self.cache_rate,
+                num_workers=self.num_workers,
+                dev=self.dev,
+                random_seed=self.random_seed,
+                split_ratio=(
+                    (1 - self.holdout_ratio) ** 2,
+                    (1 - self.holdout_ratio) * self.holdout_ratio,
+                    self.holdout_ratio,
+                ),
+                sm_as_whole=True,
+                background_classes=self.test_background_classes,
+            )
+
+
+class SmatMrDataset(Dataset):
+    def __init__(self, *args, sequence: str, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.num_classes != 4:
+            raise ValueError(f"There are 4 classes in SMAT dataset. Got {self.num_classes}")
+
+        if self.in_use:
+            self.train_dataset = _SmatMrDatasetWithBackgroundInfo(
+                root_dir=self.root_dir,
+                sequence=sequence,
+                target="all",
+                stage="train",
+                transform=self.train_transform,
+                mask_mapping={c: 0 for c in self.train_background_classes},
+                cache_rate=self.cache_rate,
+                num_workers=self.num_workers,
+                dev=self.dev,
+                random_seed=self.random_seed,
+                split_ratio=(
+                    (1 - self.holdout_ratio) ** 2,
+                    (1 - self.holdout_ratio) * self.holdout_ratio,
+                    self.holdout_ratio,
+                ),
+                pkd_only=False,
+                non_pkd_only=True,
+                background_classes=self.train_background_classes,
+            )
+            setattr(self.train_dataset, "batch_size", self.train_batch_size)
+
+            self.val_dataset = _SmatMrDatasetWithBackgroundInfo(
+                root_dir=self.root_dir,
+                sequence=sequence,
+                target="all",
+                stage="validation",
+                transform=self.test_transform,
+                mask_mapping={c: 0 for c in self.train_background_classes},
+                cache_rate=self.cache_rate,
+                num_workers=self.num_workers,
+                dev=self.dev,
+                random_seed=self.random_seed,
+                split_ratio=(
+                    (1 - self.holdout_ratio) ** 2,
+                    (1 - self.holdout_ratio) * self.holdout_ratio,
+                    self.holdout_ratio,
+                ),
+                pkd_only=False,
+                non_pkd_only=True,
+                background_classes=self.train_background_classes,
+            )
+
+            self.test_dataset = _SmatMrDatasetWithBackgroundInfo(
+                root_dir=self.root_dir,
+                sequence=sequence,
+                target="all",
+                stage="test",
+                transform=self.test_transform,
+                mask_mapping={c: 0 for c in self.test_background_classes},
+                cache_rate=self.cache_rate,
+                num_workers=self.num_workers,
+                dev=self.dev,
+                random_seed=self.random_seed,
+                split_ratio=(
+                    (1 - self.holdout_ratio) ** 2,
+                    (1 - self.holdout_ratio) * self.holdout_ratio,
+                    self.holdout_ratio,
+                ),
+                pkd_only=False,
+                non_pkd_only=True,
+                background_classes=self.test_background_classes,
+            )
---
+++ lib/datasets/smat_default_transform.py
@@ -0,0 +1,46 @@
+import cv2
+from medaset.image_readers import CV2Reader
+from monai.data import PydicomReader
+from monai.transforms import (
+    Compose,
+    CropForegroundd,
+    EnsureChannelFirstd,
+    LoadImaged,
+    Resized,
+    ScaleIntensityRanged,
+    SpatialCropd,
+    SpatialPadd,
+    ToTensord,
+)
+
+ct_transform = Compose(
+    [
+        LoadImaged(
+            keys=["image", "label"],
+            reader=[PydicomReader(swap_ij=False), CV2Reader(flags=cv2.IMREAD_GRAYSCALE)],
+        ),
+        EnsureChannelFirstd(keys=["image", "label"], channel_dim="no_channel"),
+        ScaleIntensityRanged(keys=["image"], a_min=-200, a_max=200, b_min=0, b_max=1, clip=True),
+        CropForegroundd(keys=["image", "label"], source_key="label"),
+        SpatialPadd(keys=["image", "label"], spatial_size=(512, 512)),
+        SpatialCropd(keys=["image", "label"], roi_center=(256, 256), roi_size=(512, 512)),
+        ToTensord(keys=["image", "label"]),
+    ]
+)
+
+
+mr_transform = Compose(
+    [
+        LoadImaged(
+            keys=["image", "label"],
+            reader=[PydicomReader(swap_ij=False, force=True), CV2Reader(flags=cv2.IMREAD_GRAYSCALE)],
+            image_only=True,
+        ),
+        EnsureChannelFirstd(keys=["image", "label"], channel_dim="no_channel"),
+        ScaleIntensityRanged(keys=["image"], a_min=0, a_max=1000, b_min=0, b_max=1, clip=True),
+        CropForegroundd(keys=["image", "label"], source_key="label"),
+        Resized(keys=["image", "label"], spatial_size=512, size_mode="longest", mode="nearest-exact"),
+        SpatialPadd(keys=["image", "label"], spatial_size=(512, 512)),
+        ToTensord(keys=["image", "label"]),
+    ]
+)
