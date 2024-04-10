b1bd939a72e6c827220f69096d8f0c64aa59f47a
Kevin Hong <kevinkevin556@yahoo.com.tw>
2024-04-10 15:31:22 +0800

    feat: create a class for entropy loss

---
+++ lib/loss/entropy_loss.py
@@ -0,0 +1,30 @@
+import numpy as np
+import torch
+from torch.nn.modules.loss import _Loss
+
+
+def get_entropy_map(v, eps=0, keepdim=True, logits=True):
+    """
+    Entropy map for probabilistic prediction vectors
+    input: batch_size x channels x h x w
+    output: batch_size x 1 x h x w
+    """
+    n, c, h, w = v.size()
+    if logits:
+        p = torch.softmax(v, dim=1)
+        log_p = torch.log_softmax(v, dim=1) / np.log(2)
+    else:
+        p = v
+        log_p = torch.log2(v + eps)
+    ent_map = torch.sum(torch.mul(p, log_p), dim=1, keepdim=keepdim) / np.log2(c)
+    return ent_map
+
+
+class EntropyLoss(_Loss):
+    def __init__(self, eps=1e-30, logits=True):
+        super().__init__()
+        self.eps = eps
+        self.logits = logits
+
+    def forward(self, x):
+        return torch.mean(get_entropy_map(x, eps=self.eps, logits=self.logits))
