4efccde66c38f97f15b045118951b6c9f22c5639
Kevin Hong <kevinkevin556@yahoo.com.tw>
2024-04-21 17:17:30 +0800

    feat: rewrite monai basic_unet classes to conform to the segmentation model def in smp

---
+++ networks/unet/__init__.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+from collections.abc import Sequence
+
+from torch import nn
+
+from networks.base import SegmentationModel
+from networks.unet.basic_unet import BasicUNetDecoder, BasicUNetEncoder, BasicUNetIdSkip
+from networks.unet.basic_unet_noskip import (
+    BasicUNetNoSkip,
+    BasicUNetNoSkipDecoder,
+    BasicUNetNoSkipEncoder,
+)
+from networks.unet.basic_unet_zeroskip import (
+    BasicUNetZeroSkip,
+    BasicUNetZeroSkipDecoder,
+    BasicUNetZeroSkipEncoder,
+)
+
+
+class BasicUNet(SegmentationModel):
+    """An smp-ized monai BasicUnet"""
+
+    def __init__(
+        self,
+        spatial_dims: int = 3,
+        in_channels: int = 1,
+        out_channels: int = 2,
+        features: Sequence[int] = (32, 32, 64, 128, 256, 32),
+        act: str | tuple = ("LeakyReLU", {"negative_slope": 0.1, "inplace": True}),
+        norm: str | tuple = ("instance", {"affine": True}),
+        bias: bool = True,
+        dropout: float | tuple = 0.0,
+        upsample: str = "deconv",
+        skip_connections: int | None = 1,
+    ):
+        if skip_connections is None:
+            self.encoder = BasicUNetNoSkipEncoder(spatial_dims, in_channels, features, act, norm, bias, dropout)
+            self.decoder = BasicUNetNoSkipDecoder(
+                spatial_dims, out_channels, features, act, norm, bias, dropout, upsample
+            )
+        elif skip_connections == 0:
+            self.encoder = BasicUNetZeroSkipEncoder(spatial_dims, in_channels, features, act, norm, bias, dropout)
+            self.decoder = BasicUNetZeroSkipDecoder(
+                spatial_dims, out_channels, features, act, norm, bias, dropout, upsample
+            )
+        elif skip_connections == 1:
+            self.encoder = BasicUNetEncoder(spatial_dims, in_channels, features, act, norm, bias, dropout)
+            self.decoder = BasicUNetDecoder(spatial_dims, out_channels, features, act, norm, bias, dropout, upsample)
+        else:
+            raise ValueError(f"skip_connection take a value among {0, 1, None}.")
+        self.segmentation_head = nn.Identity()
